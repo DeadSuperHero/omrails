2f71eea27d00ffc019f94aba7f926f039ebcc806
DeadSuperHero <sean@deadsuperhero.com>
2013-08-26 18:37:39 -0500

    Pretty up devise forms, user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-   attr_accessible :email, :password, :password_confirmation
+   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
    validates_presence_of :name
 end
