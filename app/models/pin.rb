002e4745f22554e08f12151597674db2d942f5e7
DeadSuperHero <sean@deadsuperhero.com>
2013-08-31 23:21:55 -0500

    Suck it Mattan, I'm using CarrierWave instead of PaperClip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,8 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description, :image, :remote_image_url
 
   validates :description, presence: true
   belongs_to :user
   validates :user_id, presence: true
+  mount_uploader :image, ImageUploader
 end
