3b2872dc76041eb26fff1dc4619062572714e981
DeadSuperHero <sean@deadsuperhero.com>
2013-08-31 22:30:02 -0500

    Add pin association with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
    attr_accessible :name, :email, :password, :password_confirmation, :remember_me
    validates_presence_of :name
+   has_many :pins, :dependent => :destroy
 end
